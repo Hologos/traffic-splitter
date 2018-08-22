2683e58fc83d6a4497fec56adf10148aaddbfd90
Jiří Málek <jiri.malek@gmail.com>
2018-08-22 14:43:52 +0200

    Splits getting network interface details into multiple functions.

--- libs/network.sh
+++ libs/network.sh
@@ -8,17 +8,40 @@ function verify_network_interfaces()
         exception 1 "Tunnel interface is not specified."
     fi
 
-    for interface in "${INTERFACE_DEFAULT}" "${INTERFACE_TUNNEL}"; do
-        interface_setup="$(ifconfig "${interface}" 2> /dev/null || echo "")"
+    for interface_name in "${INTERFACE_DEFAULT}" "${INTERFACE_TUNNEL}"; do
+        interface_setup="$(get_network_interface_details "${interface_name}")"
+        interface_status="$(get_network_interface_status "${interface_name}")"
 
         if [[ "${interface_setup}" == "" ]]; then
             exception 1 "Interface ${interface} doesn't exist."
         fi
 
-        if [[ "$(echo "${interface_setup}" | egrep '^\s+status: active$')" == "" ]]; then
-            exception 1 "Interface ${interface} is shut down."
+        if [[ "${interface_status}" != "active" ]]; then
+            exception 1 "Interface ${interface_name} is not online."
         fi
     done
+}
+
+function get_network_interface_details()
+{
+    if [[ $# -ne 1 ]]; then
+        exception 1 "Improper function call: ${FUNCNAME[0]} <interface-name>"
+    fi
+
+    interface_name="$1"
+
+    ifconfig "${interface_name}" 2> /dev/null || echo ""
+}
+
+function get_network_interface_status()
+{
+    if [[ $# -ne 1 ]]; then
+        exception 1 "Improper function call: ${FUNCNAME[0]} <interface-name>"
+    fi
+
+    interface_name="$1"
+
+    get_network_interface_details "${interface_name}" | egrep '^\s+status:' | cut -d" " -f 2 || echo ""
 }
 
 function restart_network_interfaces()
