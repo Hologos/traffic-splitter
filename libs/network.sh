bb0b1659573a15026afb131851d68638a08f3f07
Jiří Málek <jiri.malek@gmail.com>
2018-08-23 08:59:42 +0200

    Detects interfaces coming online after restart (closes #2).

--- libs/network.sh
+++ libs/network.sh
@@ -46,15 +46,95 @@ function get_network_interface_status()
 
 function restart_network_interfaces()
 {
-    echo "Restarting network interfaces."
+    echo
+    echo "${FORMAT_BOLD}Restarting network interfaces${FORMAT_NORMAL}"
+
+    echo -n "   "
+    echo "Shutting down interface ${INTERFACE_DEFAULT}."
+    echo -n "   "
+    echo "Shutting down interface ${INTERFACE_TUNNEL}."
 
     ifconfig "${INTERFACE_DEFAULT}" down
     ifconfig "${INTERFACE_TUNNEL}" down
+
     sleep 2
+
+    echo -n "   "
+    echo "Starting up interface ${INTERFACE_DEFAULT}."
+    echo -n "   "
+    echo "Starting up interface ${INTERFACE_TUNNEL}."
+
     ifconfig "${INTERFACE_DEFAULT}" up
     ifconfig "${INTERFACE_TUNNEL}" up
 
-    # TODO: create function check_network_status, which checks if interfaces are already online and connected
-    #       or maybe checks netstat if default-gateway exists for specified interface.
-    sleep 5
+    check_network_status
+}
+
+function check_network_status()
+{
+    echo
+    echo "${FORMAT_BOLD}Restarting network interfaces${FORMAT_NORMAL}"
+
+    checked_network_interface="${INTERFACE_DEFAULT}"
+
+    local interface_statuses=()
+    local counter=0
+
+    while true; do
+        local index=0
+        for checked_network_interface in "${INTERFACE_DEFAULT}" "${INTERFACE_TUNNEL}"; do
+            interface_status="$(get_network_interface_status "${checked_network_interface}")"
+            interface_status_formatted="${FORMAT_FOREGROUND_RED}unknown${FORMAT_NORMAL}"
+
+            if [[ "${interface_status}" == "" ]]; then
+                exception 1 "There is a problem with interface ${checked_network_interface}."
+            fi
+
+            case "${interface_status}" in
+                inactive)
+                    interface_status_formatted="${FORMAT_FOREGROUND_RED}offline${FORMAT_NORMAL}"
+                ;;
+
+                active)
+                    interface_gateway="$(get_gateway_of_interface "${checked_network_interface}")"
+
+                    if [[ "${interface_gateway}" == "" ]]; then
+                        interface_status="not-connected"
+                        interface_status_formatted="${FORMAT_FOREGROUND_BLUE}not connected${FORMAT_NORMAL}"
+                    else
+                        interface_status_formatted="${FORMAT_FOREGROUND_GREEN}online${FORMAT_NORMAL}"
+                    fi
+                ;;
+
+                *)
+                    exception 1 "Unknown interface status '${interface_status}' of interface ${checked_network_interface}."
+            esac
+
+            interface_statuses[${index}]="${interface_status}"
+
+            echo -n "   "
+            echo "Interface ${checked_network_interface} status: ${interface_status_formatted}"
+
+            index=$(( ${index} + 1 ))
+        done
+
+        # end here
+        if [[ "$(echo "${interface_statuses[@]}" | grep -v 'not-connected' | grep -v 'inactive' | grep -v 'grep')" != "" ]]; then
+            break
+        fi
+
+        sleep 1
+
+        counter=$(( ${counter} + 1 ))
+
+        # safe break
+        if [[ ${counter} -gt 30 ]]; then
+            exception 1 "Waiting for network interfaces coming online timed out."
+        fi
+
+        echo -ne "${FORMAT_UP_ONE_LINE}"
+        echo -ne "${FORMAT_CLEAR_TO_EOL}"
+        echo -ne "${FORMAT_UP_ONE_LINE}"
+        echo -ne "${FORMAT_CLEAR_TO_EOL}"
+    done
 }
